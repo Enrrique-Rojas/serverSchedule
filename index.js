a0556771e24ea9622423add87e04da4e0120a8f7
Enrrique-Rojas <113274276+Enrrique-Rojas@users.noreply.github.com>
2022-11-22 04:26:02 -0500

    SUBIDA

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const schedule = require('node-schedule');
+const axios = require('axios');
+app.use('*', cors());
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    const someDate = new Date(req.body.date)
+    const data = {
+        'tipoUsuario':req.body.tipoUsuario,
+        'package':req.body.package,
+        'mensaje':req.body.mensaje,
+        'enviadoPor':req.body.enviadoPor,
+        'idUsuario':req.body.idUsuario,
+        'idSala':req.body.idSala,
+    };
+    schedule.scheduleJob(someDate,()=>{
+        console.log('IMPRIMIENDO METODO',req.body.date)
+        axios.post("https://tuclinika.net/api/notificacionpush",data).then(res=>{
+            console.log('STATUS CODE:',res.status)
+            console.log('BODY:',res.data)
+        }).catch(err=>{
+            console.log(err)
+        })
+    })
+    res.send("API is up.");
+})
+
+app.listen(8080, () => {
+    console.log('API is running');
+})
+
